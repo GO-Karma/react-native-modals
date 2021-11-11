6eb262c68aa6868fda2faf85adee378416b4d3b0
Aykut Saki <aykutsakisocial@gmail.com>
2021-11-11 21:54:03 +0300

    test

--- src/components/ModalButton.js
+++ src/components/ModalButton.js
@@ -1,33 +1,34 @@
 // @flow
 
-import React from 'react';
-import { Text, PixelRatio, TouchableHighlight, StyleSheet, Platform } from 'react-native';
-import { Positions } from '../constants/Constants';
-import type { ModalButtonProps } from '../type';
+import React from "react";
+import { Text, PixelRatio, StyleSheet, Platform } from "react-native";
+import { Positions } from "../constants/Constants";
+import type { ModalButtonProps } from "../type";
+import { TouchableHighlight } from "react-native-gesture-handler";
 
-const isAndroid = Platform.OS === 'android';
+const isAndroid = Platform.OS === "android";
 
 const styles = StyleSheet.create({
   button: {
     flex: 1,
-    width: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
     paddingTop: 16,
     paddingBottom: 16,
   },
   border: {
-    borderLeftColor: '#CCD0D5',
+    borderLeftColor: "#CCD0D5",
     borderLeftWidth: 1 / PixelRatio.get(),
   },
   text: {
-    fontWeight: isAndroid ? '400' : '500',
-    fontFamily: isAndroid ? 'sans-serif-medium' : 'System',
+    fontWeight: isAndroid ? "400" : "500",
+    fontFamily: isAndroid ? "sans-serif-medium" : "System",
     fontSize: isAndroid ? 19 : 16,
-    color: '#044DE0',
+    color: "#044DE0",
   },
   disable: {
-    color: '#C5C6C5',
+    color: "#C5C6C5",
   },
 });
 
@@ -37,7 +38,7 @@ const ModalButton = ({
   style,
   textStyle,
   activeOpacity = 0.6,
-  align = 'center',
+  align = "center",
   disabled = false,
   bordered = false,
 }: ModalButtonProps) => {
@@ -53,9 +54,7 @@ const ModalButton = ({
       activeOpacity={activeOpacity}
       style={[styles.button, buttonAlign, border, style]}
     >
-      <Text style={[styles.text, disable, textStyle]}>
-        {text}
-      </Text>
+      <Text style={[styles.text, disable, textStyle]}>{text}</Text>
     </TouchableHighlight>
   );
 };
